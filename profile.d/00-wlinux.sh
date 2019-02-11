2b54e33b59ea48d036184d9b06382d845f888983
Kim Bradley <kim.jamie.bradley@gmail.com>
2019-02-11 11:09:57 +0000

    Remove /etc/profile and move code to /etc/profile.d/00-wlinux.sh
    Signed-off-by: Kim Bradley <kim.jamie.bradley@gmail.com>

---
+++ profile.d/00-wlinux.sh
@@ -0,0 +1,25 @@
+# WSL
+IS_WSL=`grep -i microsoft /proc/version`
+if test "$IS_WSL" = ""; then
+  if [ "`id -u`" -eq 0 ]; then
+    PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+  else
+    PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
+  fi
+fi
+export PATH
+
+# enable external x display
+export DISPLAY=:0
+
+# enable external libgl
+export LIBGL_ALWAYS_INDIRECT=1
+
+# speed up some GUI apps like gedit
+export NO_AT_BRIDGE=1
+
+# Execute on user's shell first-run
+if [ ! -f "${HOME}/.firstrun" ]; then
+    echo "Welcome to WLinux. Type 'wlinux-setup' to run the setup tool. You will only see this $
+    touch "${HOME}/.firstrun"
+fi
