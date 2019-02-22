5c89c051e3aa296b8d7d1069e5d07db1e36dc1d1
Kim Bradley <kim.jamie.bradley@gmail.com>
2019-02-22 11:02:29 +0000

    Add alias 'clear -x' fix
    Signed-off-by: Kim Bradley <kim.jamie.bradley@gmail.com>

--- profile.d/00-wlinux.sh
+++ profile.d/00-wlinux.sh
@@ -18,6 +18,9 @@ export LIBGL_ALWAYS_INDIRECT=1
 # speed up some GUI apps like gedit
 export NO_AT_BRIDGE=1
 
+# Fix 'clear' scrolling issues
+alias clear='clear -x'
+
 # Execute on user's shell first-run
 if [ ! -f "${HOME}/.firstrun" ]; then
     echo "Welcome to WLinux. Type 'wlinux-setup' to run the setup tool. You will only see this message on first run"
