35f342f362063c2dabd468233c0ea6345d443a44
Carlos Rafael Ramirez <crramirez@gmail.com>
2019-04-04 20:27:06 -0500

    Fix winhome creation with zsh

--- profile.d/00-pengwin.sh
+++ profile.d/00-pengwin.sh
@@ -19,25 +19,10 @@ if [ ! -f "${HOME}/.firstrun" ]; then
   touch "${HOME}/.firstrun"
 fi
 
-function cmd_exe() {
-
-  local result
-  local exit_status
-  cd $(wslpath C:\\) >/dev/null
-
-  result=$(cmd.exe $@ 2>&1)
-  exit_status=$?
-
-  cd - >/dev/null
-
-  echo "${result}"
-  return ${exit_status}
-}
-
 function create_winhome_link() {
 
   # Create a symbolic link to the windows home
-  local wHomeWinPath=$(cmd_exe /c 'echo %HOMEDRIVE%%HOMEPATH%' | tr -d '\r')
+  local wHomeWinPath=$(cmd-exe /c 'echo %HOMEDRIVE%%HOMEPATH%' | tr -d '\r')
   export WIN_HOME=$(wslpath -u "${wHomeWinPath}")
 
   local win_home_lnk=${HOME}/winhome
