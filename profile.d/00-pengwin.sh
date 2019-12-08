bbc85e3eb085400ac0fe21a20237041a2b0d7e42
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2019-12-08 22:26:56 +0100

    Verify WSL2 via WSL_INTEROP env var. Simpler for now

--- profile.d/00-pengwin.sh
+++ profile.d/00-pengwin.sh
@@ -1,8 +1,7 @@
 set -x
 
 # check whether it is WSL1 for WSL2
-# check whether we have wsl.exe in path
-if ( which wsl.exe >/dev/null ) && ( $(which wsl.exe) -l -v >/dev/null ) && [ $(wsl.exe -l -v 2>&1 | iconv -f Unicode -t $(locale charmap) | sed -e "s|\r||g" -e "s|\x00||g" | grep ${WSL_DISTRO_NAME} | awk '{print $3}') -eq 2 ]; then
+if [[ -n ${WSL_INTEROP} ]]; then
   #Export an enviroment variable for helping other processes
   export WSL2=1
   # enable external x display for WSL 2
