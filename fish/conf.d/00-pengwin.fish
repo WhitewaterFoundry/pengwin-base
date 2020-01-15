05df91f5294af820ef0e03d32f0e5de6a9c96a79
laventillelion <31141228+laventillelion@users.noreply.github.com>
2020-01-15 17:12:27 -0500

    Quote another test -n argument
    Quote another test -n argument

--- fish/conf.d/00-pengwin.fish
+++ fish/conf.d/00-pengwin.fish
@@ -11,7 +11,7 @@ if test -n "$WSL_INTEROP"
   end
 
   set wsl2_d_tmp (eval $ipconfig_exec | grep -n -m 1 "Default Gateway.*: [0-9a-z]" | cut -d : -f 1)
-  if test -n $wsl2_d_tmp
+  if test -n "$wsl2_d_tmp"
     set first_line (expr $wsl2_d_tmp - 4)
     set wsl2_d_tmp (eval $ipconfig_exec | sed $first_line,$wsl2_d_tmp!d | grep IPv4 | cut -d : -f 2 | sed -e "s|\s||g" -e "s|\r||g")
     set --export DISPLAY "$wsl2_d_tmp:0.0"
