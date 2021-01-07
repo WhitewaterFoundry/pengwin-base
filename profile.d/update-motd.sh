4bdad75b1cdcd14e0c2f378c682cdd3b157819a7
javier santos <javier.david.santos@gmail.com>
2021-01-07 09:19:15 -0500

    Update messages and format

--- profile.d/update-motd.sh
+++ profile.d/update-motd.sh
@@ -6,6 +6,8 @@ hushlogin_file="$HOME/.hushlogin"
 if [ ! -e "$hushlogin_file" ] && ! find "$motd_shown_file" -newermt 'today  0:00' 2>/dev/null | grep -q  -m 1 '';  then
     run-parts  --lsbsysinit /etc/update-motd.d
     touch "$motd_shown_file"
-    printf "This message is shown once a day. To disable it you can create the\n%s file\n" "$hushlogin_file"
+    printf "\n"
+    printf "This message is shown once a day. To disable it you can create the %s file" "$hushlogin_file"
+    printf "\n\n"
 fi
 
