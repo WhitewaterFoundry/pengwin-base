462b5fe5938722d65bfcae9303cb34483321a1ff
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-04-06 12:01:07 +0200

    Added the case when DISPLAY is already defined

--- profile.d/00-pengwin.sh
+++ profile.d/00-pengwin.sh
@@ -5,56 +5,72 @@ if ! (id -Gn | grep -c "adm.*sudo\|sudo.*adm" >/dev/null); then
   return
 fi
 
-# WSL2 Environment variable meaning:
-# WSL2=0: WSL1
-# WSL2=1: WSL2 (Type 1)
-# WSL2=2: WSL2 (Type 2)
-if [ -n "${WSL_INTEROP}" ]; then
-  # enable external x display for WSL 2
+setup_display() {
+  # WSL2 Environment variable meaning:
+  # WSL2=0: WSL1
+  # WSL2=1: WSL2 (Type 1) 
+  # WSL2=2: WSL2 (Type 2)
+  # WSL2=3: WSL2 (Type 3)
+  if [ -n "${WSL_INTEROP}" ]; then
+  
+    if [ -n "${DISPLAY}" ]; then
+      # check if the type is changed
+      sudo /usr/local/bin/wsl_change_checker 3 "WSL2" "${DISPLAY}"
+      sudo /usr/local/bin/wsl2_ip_checker "$(echo "$DISPLAY" | cut -d : -f 1)"
+      #Export an enviroment variable for helping other processes
+      export WSL2=3
+      
+      return
+    fi
+    
+    # enable external x display for WSL 2
+    ipconfig_exec=$(wslpath "C:\\Windows\\System32\\ipconfig.exe")
+    if (command -v ipconfig.exe >/dev/null 2>&1); then
+      ipconfig_exec=$(command -v ipconfig.exe)
+    fi
 
-  ipconfig_exec=$(wslpath "C:\\Windows\\System32\\ipconfig.exe")
-  if (command -v ipconfig.exe >/dev/null 2>&1); then
-    ipconfig_exec=$(command -v ipconfig.exe)
-  fi
+    wsl2_d_tmp="$(eval "$ipconfig_exec 2> /dev/null" | grep -n -m 1 "Default Gateway.*: [0-9a-z]" | cut -d : -f 1)"
 
-  wsl2_d_tmp="$(eval "$ipconfig_exec 2> /dev/null" | grep -n -m 1 "Default Gateway.*: [0-9a-z]" | cut -d : -f 1)"
+    if [ -n "${wsl2_d_tmp}" ]; then
 
-  if [ -n "${wsl2_d_tmp}" ]; then
+      wsl2_d_tmp="$(eval "$ipconfig_exec" | sed "$((wsl2_d_tmp - 4))"','"$((wsl2_d_tmp + 0))"'!d' | grep IPv4 | cut -d : -f 2 | sed -e "s|\s||g" -e "s|\r||g")"
+      export DISPLAY=${wsl2_d_tmp}:0
 
-    wsl2_d_tmp="$(eval "$ipconfig_exec" | sed "$((wsl2_d_tmp - 4))"','"$((wsl2_d_tmp + 0))"'!d' | grep IPv4 | cut -d : -f 2 | sed -e "s|\s||g" -e "s|\r||g")"
-    export DISPLAY=${wsl2_d_tmp}:0
+      # check if the type is changed
+      sudo /usr/local/bin/wsl_change_checker 2 "WSL2" "${wsl2_d_tmp}:0\.0"
+      sudo /usr/local/bin/wsl2_ip_checker "$wsl2_d_tmp"
+      #Export an enviroment variable for helping other processes
+      export WSL2=2
 
-    # check if the type is changed
-    sudo /usr/local/bin/wsl_change_checker 2 "WSL2" "${wsl2_d_tmp}:0\.0"
-    sudo /usr/local/bin/wsl2_ip_checker "$wsl2_d_tmp"
-    #Export an enviroment variable for helping other processes
-    export WSL2=2
+    else
+      wsl2_d_tmp="$(grep </etc/resolv.conf nameserver | awk '{print $2}')"
+      export DISPLAY=${wsl2_d_tmp}:0
+
+      # check if we have wsl.exe in path
+      sudo /usr/local/bin/wsl_change_checker 1 "WSL2" "$DISPLAY"
+      sudo /usr/local/bin/wsl2_ip_checker "$wsl2_d_tmp"
+      #Export an enviroment variable for helping other processes
+      export WSL2=1
+    fi
+
+    unset ipconfig_exec
+    unset wsl2_d_tmp
 
   else
-    wsl2_d_tmp="$(grep </etc/resolv.conf nameserver | awk '{print $2}')"
-    export DISPLAY=${wsl2_d_tmp}:0
+
+    # enable external x display for WSL 1
+    export DISPLAY=localhost:0
 
     # check if we have wsl.exe in path
-    sudo /usr/local/bin/wsl_change_checker 1 "WSL2" "$DISPLAY"
-    sudo /usr/local/bin/wsl2_ip_checker "$wsl2_d_tmp"
-    #Export an enviroment variable for helping other processes
-    export WSL2=1
+    sudo /usr/local/bin/wsl_change_checker 0 "WSL1" "localhost:0"
+
+    # Export an enviroment variable for helping other processes
+    unset WSL2
+
   fi
+}
 
-  unset wsl2_d_tmp
-  unset ipconfig_exec
-else
-
-  # enable external x display for WSL 1
-  export DISPLAY=localhost:0
-
-  # check if we have wsl.exe in path
-  sudo /usr/local/bin/wsl_change_checker 0 "WSL1" "localhost:0"
-
-  # Export an enviroment variable for helping other processes
-  unset WSL2
-
-fi
+setup_display
 
 # enable external libgl if mesa is not installed
 if (command -v glxinfo >/dev/null 2>&1); then
