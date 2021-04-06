a73accd378e7f775b3a04c557ea1cf301214f9b1
Carlos Ramirez <crramirez@gmail.com>
2021-04-06 23:48:34 +0200

    In fish verify if DISPLAY is already defined

--- fish_conf.d/00-pengwin.fish
+++ fish_conf.d/00-pengwin.fish
@@ -1,27 +1,31 @@
+#!/bin/fish
 
 # check whether it is WSL1 for WSL2
 if test -n "$WSL_INTEROP"
   #Export an enviroment variable for helping other processes
   set --export WSL2 1
-  # enable external x display for WSL 2
 
-  set ipconfig_exec (wslpath "C:\\Windows\\System32\\ipconfig.exe")
-  if which ipconfig.exe >/dev/null
-    set ipconfig_exec (which ipconfig.exe)
+  if test -z "$DISPLAY"
+    # enable external x display for WSL 2
+
+    set ipconfig_exec (wslpath "C:\\Windows\\System32\\ipconfig.exe")
+    if which ipconfig.exe >/dev/null
+      set ipconfig_exec (which ipconfig.exe)
+    end
+
+    set wsl2_d_tmp (eval $ipconfig_exec | grep -n -m 1 "Default Gateway.*: [0-9a-z]" | cut -d : -f 1)
+    if test -n "$wsl2_d_tmp"
+      set first_line (expr $wsl2_d_tmp - 4)
+      set wsl2_d_tmp (eval $ipconfig_exec | sed $first_line,$wsl2_d_tmp!d | grep IPv4 | cut -d : -f 2 | sed -e "s|\s||g" -e "s|\r||g")
+      set --export DISPLAY "$wsl2_d_tmp:0"
+      set -e first_line
+    else
+      set --export DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+    end
+
+    set -e wsl2_d_tmp
+    set -e ipconfig_exec
   end
-
-  set wsl2_d_tmp (eval $ipconfig_exec | grep -n -m 1 "Default Gateway.*: [0-9a-z]" | cut -d : -f 1)
-  if test -n "$wsl2_d_tmp"
-    set first_line (expr $wsl2_d_tmp - 4)
-    set wsl2_d_tmp (eval $ipconfig_exec | sed $first_line,$wsl2_d_tmp!d | grep IPv4 | cut -d : -f 2 | sed -e "s|\s||g" -e "s|\r||g")
-    set --export DISPLAY "$wsl2_d_tmp:0"
-    set -e first_line
-  else
-    set --export DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-  end
-
-  set -e wsl2_d_tmp
-  set -e ipconfig_exec
 else
   # enable external x display for WSL 1
   set --export DISPLAY "localhost:0"
